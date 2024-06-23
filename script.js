7ab080ddbb3328215906cdf73e51522516de74df
Izaan Khan <izaankhan.x@gmail.com>
2024-06-23 09:13:15 +0500

    added smooth scroll and new gifs to service card bg

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Autoplay was prevented:', error);
     });
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
